b03d83251370f418d9aa4678e92799d46eaa7313
LeoTuet <leopold.tueting@gmail.com>
2022-02-05 15:54:58 +0100

    updates path

--- scripts/pull-current-images-and-recreate.sh
+++ scripts/pull-current-images-and-recreate.sh
@@ -1,5 +1,5 @@
 docker pull ghcr.io/leotuet/backend-we-want-to-sleep:prod
 docker pull ghcr.io/leotuet/ui-we-want-to-sleep:prod
 
-docker stack remove wwts
-docker stack deploy --compose-file=../docker/docker-compose.yml wwts-cluster
+docker stack remove wwts-cluster
+docker stack deploy --compose-file=~/deployment-we-want-to-sleep/docker/docker-compose.yml wwts-cluster
