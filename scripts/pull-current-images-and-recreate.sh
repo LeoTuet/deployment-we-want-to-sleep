750220482cfa373bba8dca9d764f88c49e17ef9e
LeoTuet <leopold.tueting@gmail.com>
2022-02-05 15:34:08 +0100

    removes stack before redeploying

--- scripts/pull-current-images-and-recreate.sh
+++ scripts/pull-current-images-and-recreate.sh
@@ -1,4 +1,5 @@
 docker pull ghcr.io/leotuet/backend-we-want-to-sleep:prod
 docker pull ghcr.io/leotuet/ui-we-want-to-sleep:prod
 
+docker stack remove wwts
 docker stack deploy --compose-file=../docker/docker-compose.yml wwts-cluster
