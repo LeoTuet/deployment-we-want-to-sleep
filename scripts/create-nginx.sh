751a204fca753776f1d7be8a469c491d8167ecff
LeoTuet <leopold.tueting@gmail.com>
2022-02-05 15:31:34 +0100

    cleans up repo and adapts to final changes #3 #2

--- scripts/create-nginx.sh
+++
@@ -1,8 +0,0 @@
-while getopts e: flag
-do
-    case "${flag}" in
-        e) enviroment=${OPTARG};;
-    esac
-done
-
-sudo docker build --build-arg configFile="$enviroment" -t wwts_proxy ../nginx 
--- scripts/pull-current-images-and-restart.sh
+++ scripts/pull-current-images-and-restart.sh
@@ -1,5 +1,4 @@
 docker pull ghcr.io/leotuet/backend-we-want-to-sleep:prod
 docker pull ghcr.io/leotuet/ui-we-want-to-sleep:prod
 
-sh ./create-nginx.sh
-sudo docker-compose -f docker-compose.yml up -d
+docker stack deploy --compose-file=../docker/docker-compose.yml wwts-cluster
