f0e44d18c8ec6cc21f1f49a6a0645c8e49f1c6ec
Leo <leopold@tueting.net>
2022-02-09 09:38:44 +0100

    removes docker prune

--- scripts/pull-current-images-and-recreate.sh
+++ scripts/pull-current-images-and-recreate.sh
@@ -2,5 +2,4 @@ docker pull ghcr.io/leotuet/backend-we-want-to-sleep:prod
 docker pull ghcr.io/leotuet/ui-we-want-to-sleep:prod
 
 docker stack remove wwts-cluster
-docker system prune -a
-docker stack deploy --compose-file=~/deployment-we-want-to-sleep/docker/docker-compose.yml wwts-cluster
+docker stack deploy -c ~/deployment-we-want-to-sleep/docker/docker-compose.yml wwts-cluster
