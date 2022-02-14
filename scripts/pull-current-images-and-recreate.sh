4564dfe60694d91b2005c51aa07bf172dba9eda8
LeoTuet <leopold@tueting.net>
2022-02-14 19:57:26 +0100

    adds sleep buffer

--- scripts/pull-current-images-and-recreate.sh
+++ scripts/pull-current-images-and-recreate.sh
@@ -2,4 +2,7 @@ docker pull ghcr.io/leotuet/backend-we-want-to-sleep:prod
 docker pull ghcr.io/leotuet/ui-we-want-to-sleep:prod
 
 docker stack remove wwts-cluster
+
+# There is a bug where a network still isn't removed when deploying the new cluster
+sleep 1
 docker stack deploy -c ~/deployment-we-want-to-sleep/docker/docker-compose.yml wwts-cluster
