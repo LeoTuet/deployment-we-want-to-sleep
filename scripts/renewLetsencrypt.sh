45f7c0600d797dfa49d8fd0b0412e94999890799
Cloud <info@wewanttosleep.de>
2022-05-12 11:57:48 +0000

    adds script for manual ssl certificate renewal

---
+++ scripts/renewLetsencrypt.sh
@@ -0,0 +1,16 @@
+docker stack remove wwts-cluster
+
+sleep 2
+
+docker run --rm \
+  -p 443:443 -p 80:80 --name letsencrypt \
+  -v "/etc/letsencrypt:/etc/letsencrypt" \
+  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+  certbot/certbot certonly -n \
+  -m "info@wewanttosleep.de" \
+  -d wewanttosleep.de \
+  --standalone --agree-tos
+
+sleep 2
+
+docker stack deploy -c ~/deployment-we-want-to-sleep/docker/docker-compose.yml wwts-cluster
