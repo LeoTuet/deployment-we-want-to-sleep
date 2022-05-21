bbdf77dbe9a1b641c8c24397bcd7196f063cb5c9
Cloud <info@wewanttosleep.de>
2022-05-21 13:10:56 +0000

    #6 changes to only deploy changeable servies

--- scripts/pull-current-images-and-recreate.sh
+++ scripts/pull-current-images-and-recreate.sh
@@ -1,8 +1,14 @@
 docker pull ghcr.io/leotuet/backend-we-want-to-sleep:prod
 docker pull ghcr.io/leotuet/ui-we-want-to-sleep:prod
 
-docker stack remove wwts-cluster
+docker service update --force wwts-cluster_wwts_api
+docker service update --force wwts-cluster_wwts_website
+
+## Depricated stuff
+
+# docker stack remove wwts-cluster
 
 # There is a bug where a network still isn't removed when deploying the new cluster
-sleep 1
-docker stack deploy -c ~/deployment-we-want-to-sleep/docker/docker-compose.yml wwts-cluster
+# sleep 1
+
+# docker stack deploy -c ~/deployment-we-want-to-sleep/docker/docker-compose.yml wwts-cluster
